7cd902d20521d12c178a0781ea9e72c83024b1d8
NicolinDamalja <damaljanicolin@gmail.com>
2022-10-23 17:25:55 +0200

    Nuove modifiche apportate

--- score.py
+++ score.py
@@ -1,24 +1,29 @@
 import torch
 from PIL import Image
 from torchvision import transforms
+import os
 
-def score(img, model_ptl ):
+
+def score(images, model_ptl):
     model = torch.jit.load(model_ptl)
     model.eval()
+    output = {}
 
-    input_image = Image.open(img)
-    preprocess = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+    for image in images:
+        input_image = Image.open(image).convert('RGB')
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
 
-    input_tensor = preprocess(input_image)
-    input_batch = input_tensor.unsqueeze(0)
-    with torch.no_grad():
-        output = model(input_batch)[0]
+        input_tensor = preprocess(input_image)
+        input_batch = input_tensor.unsqueeze(0)
+        with torch.no_grad():
+            output.update({os.path.basename(image): model(input_batch)})
 
     return output
 
 
 if __name__ == "__main__":
-    print(score('images/girl.jpeg', "data/model.ptl"))
+    images = ['images/Yes_1.png', 'images/No_1.png', 'images/Yes_2.png', 'images/No_2.png']
+    print(score(images, 'data/model.ptl'))
